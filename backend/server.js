a4ec9119fa4e8a1c436c70c923c6c99a4578b77e
Chirag S <chiragsubhash269@gmail.com>
2025-11-17 01:55:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || https://acad-sync-backend-v2fu.onrender.com;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
